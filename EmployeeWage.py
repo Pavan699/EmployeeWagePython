6a37d060014a783ee8739c0064879a7d38f00b96
Pavan699 <pavannakate03@gmail.com>
2021-11-08 13:51:27 +0530

    [Pavan] Add: Calculating the Daliy Wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -18,13 +18,21 @@ def check_emp_wage():
     Return:
         None
     """  
+    #contant
+    WAGE_PER_HOUR = 20
     #using random to check Attendance
     check = random.randint(0,1)
     # if 1-> Present is 0-> Absent
     if check == 1:
         print("Employee is present")
+        emp_work_hour = 8
     else:
         print("Employee is absent")
+        emp_work_hour = 0
+    
+    #Calculating Daliy Wage
+    daliy_wage = emp_work_hour * WAGE_PER_HOUR
+    print("Daliy Wage is : ",daliy_wage)
 
 if __name__ == "__main__":
     check_emp_wage()
