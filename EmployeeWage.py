a92381de8d64d5623a8f93754c29b6832b29ac3d
Pavan699 <pavannakate03@gmail.com>
2021-11-08 14:19:16 +0530

    [Pavan] Add: Using switcher for the Employee daliy wage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -20,19 +20,36 @@ def check_emp_wage():
     """  
     #contant
     WAGE_PER_HOUR = 20
-    #using random to check Attendance
-    check = random.randint(0,1)
+    # #using random to check Attendance
+    # check = random.randint(0,1)
     # if 1-> Present is 0-> Absent
-    if check == 1:
+    if check == full_time:
         print("Employee is Full-Time")
         emp_work_hour = 8
-    else:
+    elif check == part_time:
         print("Employee is Part-Time")
         emp_work_hour = 4
+    else:
+        print("Employee is Absent")
+        emp_work_hour = 0
     
     #Calculating Daliy Wage
     daliy_wage = emp_work_hour * WAGE_PER_HOUR
     print("Daliy Wage is : ",daliy_wage)
+
+#variables in switchers
+full_time = 0
+part_time = 1
+absent = 2
+#switche for calculating wages
+switcher = {
+    0: full_time,
+    1: part_time,
+    2: absent,
+}
+
+check = int(input("Enter 0:full-time-Wage 1:part-time-wage 2:Absent ==> "))
+emp_Check = switcher.get(check,"Invalid Input")
 
 if __name__ == "__main__":
     check_emp_wage()
