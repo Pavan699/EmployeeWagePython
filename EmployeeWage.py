f61f869b205873bcada91a2b9536524fdf015780
Pavan699 <pavannakate03@gmail.com>
2021-11-08 13:45:17 +0530

    [Pavan] Add: Checking the Employee is Present or Absent

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -6,3 +6,25 @@
 @Title : Employee Wage
 '''
 print("Welcome to Employee Wage Problem Solution")
+import random
+
+def check_emp_wage():
+    """
+    Description:
+        This function is to check whether employee is present or absent.
+        Random fuction is used to check the attendace.
+    Parameter:
+        None
+    Return:
+        None
+    """  
+    #using random to check Attendance
+    check = random.randint(0,1)
+    # if 1-> Present is 0-> Absent
+    if check == 1:
+        print("Employee is present")
+    else:
+        print("Employee is absent")
+
+if __name__ == "__main__":
+    check_emp_wage()
