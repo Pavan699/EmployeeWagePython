a1544f09f53d6165ea7a8b04f2d4d3cfb3b4f15c
Pavan699 <pavannakate03@gmail.com>
2021-11-08 13:40:26 +0530

    [Pavan] Add: Welcome Massege for the Employee-Wage

---
+++ EmployeeWage.py
@@ -0,0 +1,8 @@
+'''
+@Author: Pavan Nakate
+@Date: 2021-11-08 01:13
+@Last Modified by: Pavan Nakate
+@Last Modified time: None
+@Title : Employee Wage
+'''
+print("Welcome to Employee Wage Problem Solution")
