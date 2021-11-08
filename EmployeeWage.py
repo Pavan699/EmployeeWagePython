71ee506f0a3908ebe71cdd4e490ec928e82591f0
Pavan699 <pavannakate03@gmail.com>
2021-11-08 14:47:42 +0530

    [Pavan] Add: Calculating till condition for max-day and max-hours

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -21,9 +21,13 @@ def check_emp_wage():
     #contant
     WAGE_PER_HOUR = 20
     MAX_WORK_DAYS = 20
+    MAX_WORK_HOURS = 100
+    #variables
     total_wage = 0
+    total_working_hours = 0
+    total_working_days = 0
     
-    for day in range(MAX_WORK_DAYS):
+    while not(total_working_days == MAX_WORK_DAYS or total_working_hours == MAX_WORK_HOURS):
         # if 0-> Fulltime is 1-> Parttime 2->Absent
         check = random.randint(0,2)
         if check == full_time:
@@ -39,6 +43,10 @@ def check_emp_wage():
         #Calculating Daliy Wage
         daliy_wage = emp_work_hour * WAGE_PER_HOUR
         print("Daliy Wage is : ",daliy_wage)
+
+        #incrementing hours and days
+        total_working_days += 1
+        total_working_hours += emp_work_hour
 
         total_wage += daliy_wage
     print("Total Month Wage is : ",total_wage)
